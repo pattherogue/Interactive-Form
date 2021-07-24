77ffe0dc1cef1a3fcad57170969c0753bc71da14
Paolo Gomez <78209142+pattherogue@users.noreply.github.com>
2021-07-24 10:13:01 -0700

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,9 @@
+/* first text field focus state on page load */
+const nameField = document.getElementById('name');
+nameField.focus();
+
+/* hide "other-job-role" when form first loads */
+const otherJobRole = document.getElementById('other-job-role');
+otherJobRole.style.display = 'none';
+
+/* 
